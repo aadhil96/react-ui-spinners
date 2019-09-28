c565e79a162785363e4a9402e1927d48417e6b5f
unknown <aadhil.imam@gmail.com>
2019-09-28 17:05:43 +0530

    add rollup and babel with react

---
+++ rollup.config.js
@@ -0,0 +1,43 @@
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
+
+const production = !process.env.ROLLUP_WATCH
+const dist = 'dist'
+const bundle = 'bundle'
+
+const outputs = [
+  {
+    file: `${dist}/${bundle}.cjs.js`,
+    format: 'cjs'
+  },
+  {
+    file: `${dist}/${bundle}.esm.js`,
+    format: 'esm'
+  },
+  {
+    name: 'ReactUISpinner',
+    file: `${dist}/${bundle}.umd.js`,
+    globals: {
+      react: 'React'
+    },
+    format: 'umd'
+  }
+]
+
+const common = {
+  input: 'src/index.js',
+  external: ['react'],
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    production && terser()
+  ]
+}
+
+export default outputs.map(output => ({
+  ...common,
+  output
+}))
